87ab63d326295adba3aef2e0654ff40c88b6d971
kulyk13 <13dimon1313@gmail.com>
2021-04-06 11:53:37 +0300

    try-antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import "antd/dist/antd.css";
-import { DatePicker } from "antd";
-import { Button } from "antd";
+import { DatePicker, Button, Space, Card} from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <DatePicker />
+    <div className="app">
+      <Space direction="vertical">
+        <Space>
+          <DatePicker />
+          <Button type="primary">Primary button</Button>
+        </Space>
+        <Card title="Card" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Space>
     </div>
   );
 }
